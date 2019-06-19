9c0af840f0234f5912a767e9a7fee66ef44442fe
brendankellyodg <brendan.kelly@opendatagroup.com>
2019-06-19 13:40:12 -0600

    added rest-trip

---
+++ library/scripts/rest-trip-xgboost.sh
@@ -0,0 +1,1 @@
+curl -i -k -u fastscore:fastscore -H "Content-Type: application/json" --data-binary "@xgboost_iris_single.json" https://localhost:8000/api/1/service/engine-1/2/active/model/roundtrip/0/1
