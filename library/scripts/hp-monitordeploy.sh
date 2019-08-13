18a07fd94bea6ca0dbea766a70073c96a0d9631f
brenstenkelly <brendan.kelly.atx@gmail.com>
2019-08-13 11:41:30 -0400

    updated monitor script

--- library/scripts/hp-monitordeploy.sh
+++ library/scripts/hp-monitordeploy.sh
@@ -4,7 +4,7 @@ fastscore run xgb_model-py3 influx-mux-kafka rest
 fastscore engine inspect
 fastscore use engine-1
 fastscore engine reset
-fastscore run hp_input_monitor rest influx-mux-kafka
+fastscore run hp_input_monitor-py3 rest influx-mux-kafka
 fastscore engine inspect
 cat hp_file_in.jsons | fastscore model input
 fastscore use engine-2
