4a4179d9fea02ef8d6fca25c83782ffc6303fa4a
srvdfastscore <v.prabhakar@fmr.com>
2019-08-13 17:19:08 -0400

    added streaming for monitoring

--- library/scripts/hp-monitordeploy.sh
+++ library/scripts/hp-monitordeploy.sh
@@ -4,8 +4,8 @@ fastscore run xgb_model-py3 influx-mux-kafka rest
 fastscore engine inspect
 fastscore use engine-1
 fastscore engine reset
-fastscore run hp_input_monitor-py3 rest influx-mux-kafka
-fastscore engine inspect
-cat hp_file_in.jsons | fastscore model input
+fastscore engine put hp_file_in.jsons hp_file_input.jsons
+fastscore run hp_input_monitor-py3 hp-input-file-stream influx-mux-kafka
 fastscore use engine-2
 fastscore model output
+fastscore monitor
